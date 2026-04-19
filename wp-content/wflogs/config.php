39bae8c002fea25cca7439fbcf9f9de2b2734fd1
hngluc <nguyendanghoangluc@gmail.com>
2026-04-20 01:38:29 +0700

    Khởi tạo website mỹ phẩm GlowShop và file database

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1777220333;s:7:"authKey";s:64:"91)dYNmqQ4Nkal(/l=43gGgA!Af-JK(7EcHYXw(VZ4*U8Yl3WAw/4d% :.2QEylk";s:7:"version";s:5:"1.1.0";}
